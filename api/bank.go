2649f765412bb4a1eb1fee3e2f4dc485f6a4e50e
Dhairya Khanna <dk9582671597@gmail.com>
2024-09-09 23:12:20 +0530

    feat: routes for accounts

--- api/bank.go
+++ api/bank.go
@@ -13,10 +13,27 @@ func NewBankServer() *BankServer {
 	b := new(BankServer)
 	router := http.NewServeMux()
 	router.Handle("/create", http.HandlerFunc(b.createAccountHandler))
+	router.Handle("/get", http.HandlerFunc(b.getAccountHandler))
+	router.Handle("/update", http.HandlerFunc(b.updateAccountHandler))
+	router.Handle("/delete", http.HandlerFunc(b.deleteAccountHandler))
+	router.Handle("/transfer", http.HandlerFunc(b.transferBalanceHandler))
+
 	b.Handler = router
 	return b
 
 }
 func (b *BankServer) createAccountHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("{}")
+}
+func (b *BankServer) getAccountHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("{}")
+}
+func (b *BankServer) deleteAccountHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("{}")
+}
+func (b *BankServer) updateAccountHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("{}")
+}
+func (b *BankServer) transferBalanceHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("{}")
 }
